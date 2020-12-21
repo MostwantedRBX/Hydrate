987b083cc331ab171716d3841d17089cf01a41d3
MostwantedRBX <mostwantedrbxsteam@gmail.com>
2020-12-21 04:25:46 -0600

    Added Ace Libs

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
-print("Hello, World")
+Hydrate = LibStub("AceAddon-3.0"):NewAddon("Hydrate!", "AceConsole-3.0")
+
+function Hydrate:OnInitialize()
+
+end
+
+function Hydrate:OnEnable()
+    self:Print("Hello World!") -- Testing if its Working
+end
+
+function Hydrate:OnDisable()
+    
+end
