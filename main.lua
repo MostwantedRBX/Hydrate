81a08b0620e9986da055f78c562fae0931735741
MostwantedRBX <mostwantedrbxsteam@gmail.com>
2020-12-21 08:06:35 -0600

    Functional Chat commands, But trying to get it to call the proper functions.

--- main.lua
+++ main.lua
@@ -25,7 +25,12 @@ local options = {
             name = "Show on Screen",
             desc = "Toggles the display of the message on the screen.",
             get = "IsShowOnScreen",
-            set = "ToggleShowOnScreen"
+            set = "ToggleShowOnScreen",
+        },
+        open = {
+            name = "Do It",
+            type = "execute",
+            func = function()InterfaceOptionsFrame_OpenToCategory("Hydrate!") InterfaceOptionsFrame_OpenToCategory("Hydrate!")end,
         },
     },
 }
@@ -33,13 +38,15 @@ local options = {
 
 
 function Hydrate:OnInitialize()
-    LibStub("AceConfig-3.0"):RegisterOptionsTable("Hydrate!", options, {"hydrate","hy"})
-    self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Hydrate!","Hydrate!")
     self.RegisterChatCommand("hy","ChatCommand")
     self.RegisterChatCommand("hydrate","ChatCommand")
 
-    Hydrate.showInChat = false
-    Hydrate.showOnScreen = true
+    LibStub("AceConfig-3.0"):RegisterOptionsTable("Hydrate!", options, {"hydrate","hy"})
+    self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Hydrate!","Hydrate!")
+
+
+    -- Hydrate.showInChat = false
+    -- Hydrate.showOnScreen = true
     Hydrate.message = "Make sure to Hydrate!"
 end
 
@@ -52,30 +59,33 @@ function Hydrate:OnDisable()
 
 end
 
-function Hydrate:ChatCommand(input)
+function Hydrate:ChatCommand(input) -- trying to debug this
     self:Print(self.message)
-    if not input or input:trim() == "" then
-        InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+    InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+    --[[if not input or input:trim() == "" then
+        --InterfaceOptionsFrame_OpenToCategory("Hydrate!")
+    elseif input then
+        --InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
     else
         LibStub("AceConfigCmd-3.0"):HandleCommand("hy", "Hydrate!", input)
-    end
+    end]]
 end
+-- Don't need these yet.
+-- function Hydrate:IsShowInChat(info)
+--     return self.showInChat
+-- end
 
-function Hydrate:IsShowInChat(info)
-    return self.showInChat
-end
+-- function Hydrate:ToggleShowInChat(info, value)
+--     self.showInChat = value
+-- end
 
-function Hydrate:ToggleShowInChat(info, value)
-    self.showInChat = value
-end
+-- function Hydrate:IsShowOnScreen(info)
+--     return self.showOnScreen
+-- end
 
-function Hydrate:IsShowOnScreen(info)
-    return self.showOnScreen
-end
-
-function Hydrate:ToggleShowOnScreen(info, value)
-    self.showOnScreen = value
-end
+-- function Hydrate:ToggleShowOnScreen(info, value)
+--     self.showOnScreen = value
+-- end
 
 function Hydrate:GetMessage(info)
     return self.message
